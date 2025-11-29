acf4d55926fa331fe22adc80ad2ede08d512ad3a
Chandu <chanduvijayak@gmail.com>
2025-11-29 13:58:22 +0530

    dev-branch_Logic for Weather data

--- Scripts/weather_data.py
+++ Scripts/weather_data.py
@@ -42,15 +42,3 @@ def get_weather_data(city,api_key):
     else:
         print("API Key is missing, Printing simulated data")
         return get_simulated_data(city)
-
-# city = input("Enter city: ")
-# print("\n--- Weather for:", city, "---")
-#
-# weather_data = get_weather_data(city,API_KEY)
-# dt = weather_data["dt"]
-# readable_time = datetime.fromtimestamp(dt).strftime("%Y-%m-%d %H:%M:%S")
-# print("API Timestamp:", readable_time)
-# print("Temperature:", weather_data["main"]["temp"], "°C")
-# print("Humidity:", weather_data["main"]["humidity"], "%")
-# print("Pressure:", weather_data["main"]["pressure"], "hPa")
-# print("Wind:", weather_data["wind"]["speed"], "m/s")

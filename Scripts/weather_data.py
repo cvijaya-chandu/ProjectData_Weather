640e7eaae2d5cc366bcc35932eb31caf2539499b
Chandu <chanduvijayak@gmail.com>
2025-11-29 14:28:55 +0530

    dev-branch_Logic for Weather data

--- Scripts/weather_data.py
+++ Scripts/weather_data.py
@@ -5,11 +5,19 @@ from datetime import datetime
 API_KEY = "e594d7f8c36c7d8ba95ce1f167dd7f31"
 INTERVAL_SECONDS = 10
 URL = "https://api.openweathermap.org/data/2.5/weather"
+
+"""Simulated Weather Data"""
 SIM_TEMP = 25.0
 SIM_HUMIDITY = 60
 SIM_PRESSURE = 1010
 SIM_WIND = 5.0
 def get_weather_from_api(city,key):
+    """
+    Fetch the live weather data from the OpenwaetherMap API
+    Parameters:
+        city(str): City name
+        key(str): API key to authenticate
+    """
     params = {
         'q': city,
         'appid': key,
@@ -22,6 +30,11 @@ def get_weather_from_api(city,key):
     return data
 
 def get_simulated_data(city):
+    """
+    Fetch the simulated weather data
+    Parameters:
+        city(str): City name
+    """
     now = datetime.now()
     dt_val = int(now.timestamp())
     return {
@@ -37,6 +50,12 @@ def get_simulated_data(city):
         }
 
 def get_weather_data(city,api_key):
+    """
+    Weather data function
+    Parameters:
+        city(str): City name
+        api_key(str): API key to authenticate
+    """
     if api_key:
         return get_weather_from_api(city, api_key)
     else:
